a007eb9ae389e4c49664552aaced2e0ab46101da
Sahithi-kasarla <sahithikasarla123@gmail.com>
2024-01-04 20:22:59 +0530

    Phase3EndProject

---
+++ Phase3EndProject/Data/Project3Context.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Phase3EndProject.Models;
+
+namespace Phase3EndProject.Data
+{
+    public class Project3Context : DbContext
+    {
+        public Project3Context (DbContextOptions<Project3Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Phase3EndProject.Models.DeptMaster> DeptMaster { get; set; } = default!;
+
+        public DbSet<Phase3EndProject.Models.EmpProfile>? EmpProfile { get; set; }
+    }
+}
